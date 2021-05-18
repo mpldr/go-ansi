58192f83cebc37b6a0990321fc3a75a5e41bdd27
Moritz Poldrack <ich@moritz-poldrack.de>
2021-05-18 10:07:52 +0200

    added possibility of re-enabling color

--- no-color.go
+++ no-color.go
@@ -7,3 +7,9 @@ var nocolorIsSet bool
 func init() {
 	_, nocolorIsSet = os.LookupEnv("NO_COLOR")
 }
+
+// OverrideNoColor allows re-enabling color codes if the user explicitly enabled
+// it by means of a configuration entry or commandline flag.
+func OverrideNoColor() {
+	nocolorIsSet = false
+}
