6db2a890f26cf3133be1fed40c946ed5964a8ef3
Moritz Poldrack <git@moritz.sh>
2022-04-07 14:31:14 +0200

    updated length to work with links and notifications

--- length.go
+++ length.go
@@ -6,6 +6,6 @@ import (
 )
 
 func GetLengthWithoutCodes(content string) int {
-	var re = regexp.MustCompile(`(?m)\x1b\[([\d;]+m|[\d;]+(H|A|B|C|D|J|K|S|T)|s|u)`)
+	re := regexp.MustCompile(`(?m)\x1b(\[([\d;]+m|[\d;]+(H|A|B|C|D|J|K|S|T)|s|u)|\](8;;[^\x1b]*\x1b\\|777;notify;[^;]*;[^\a]*\a))`)
 	return utf8.RuneCountInString(re.ReplaceAllString(content, ""))
 }
